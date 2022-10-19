314d9032c656bb706f62cea9cf697a269f777fe0
Ezra Free <ezrafree@users.noreply.github.com>
2022-10-18 23:04:55 -0600

    chore(package): implement pwa

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+window.onload = () => {
+  "use strict";
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
+  }
+};
+
 var toggle = function (elem) {
   elem.classList.toggle("is-visible");
   document.body.classList.toggle("modal-open");
@@ -10,3 +18,17 @@ document
       toggle(document.querySelector("#root > header > nav"));
     }
   });
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(function (registration) {
+      console.log(
+        "ServiceWorker registration successful with scope:",
+        registration.scope
+      );
+    })
+    .catch(function (error) {
+      console.log("ServiceWorker registration failed:", error);
+    });
+}
